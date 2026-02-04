21a44b4a3eb4936156a70b83d8a679b03cd324a8
BayuuPermana <bayu1234p@gmail.com>
2026-02-04 08:29:17 +0700

    fix(router): make base path conditional for dev/prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [react(), tailwindcss()],
-    base: '/Bayu/',
-})
+    base: command === 'serve' ? '/' : '/Bayu/',
+}))
