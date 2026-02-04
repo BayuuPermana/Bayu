a7092963280c4ad53bde1b257fde047c154c0471
BayuuPermana <bayu1234p@gmail.com>
2026-02-04 08:19:33 +0700

    feat(blog): setup routing and placeholder pages

---
+++ pages/BlogList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogList = () => {
+    return (
+        <div className="pt-24 px-6 container mx-auto">
+            <h1 className="text-4xl font-bold mb-8">Blog</h1>
+            <p>Coming soon...</p>
+        </div>
+    );
+};
+
+export default BlogList;
---
+++ pages/BlogPost.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPost = () => {
+    return (
+        <div className="pt-24 px-6 container mx-auto">
+            <h1 className="text-4xl font-bold mb-8">Blog Post</h1>
+            <p>Coming soon...</p>
+        </div>
+    );
+};
+
+export default BlogPost;
